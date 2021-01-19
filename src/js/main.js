1f894c68d24390eec7023e0538363ad0a4416500
macos <macos@MacBook-Pro-user.local>
2021-01-19 07:26:56 +0300

    partprice

--- src/js/main.js
+++ src/js/main.js
@@ -42,6 +42,66 @@ $(document).ready(() => {
     responsive: true,
     searching: false
   })
+  $(`#partpriceTable`).DataTable({
+    deferRender: true,
+    responsive: {
+      breakpoints: [
+        {
+          name: `desktop`,
+          width: Infinity
+        },
+        {
+          name: `tablet`,
+          width: 1024
+        },
+        {
+          name: `fablet`,
+          width: 768
+        },
+        {
+          name: `phone`,
+          width: 400
+        }
+      ]
+    },
+    responsive: true,
+    columnDefs: [
+      {
+        responsivePriority: 1,
+        targets: 0
+      },
+      {
+        responsivePriority: 4,
+        targets: 1
+      },
+      {
+        responsivePriority: 6,
+        targets: 2
+      },
+      {
+        responsivePriority: 5,
+        targets: 3
+      },
+      {
+        responsivePriority: 7,
+        targets: 4
+      },
+      {
+        responsivePriority: 3,
+        targets: 5
+      },
+      {
+        responsivePriority: 1,
+        targets: 6
+      },
+     
+    ],
+    processing: true,
+    paging: false,
+    searching: false,
+    info: false
+  })
+
   Inputmask(`99-999-99`).mask(`.phone-mask`)
   $(`.card-delivery`).click(function () {
     $(`.card-delivery`).removeClass(`border-primary`)
